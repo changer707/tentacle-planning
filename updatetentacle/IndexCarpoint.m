d17e1c57aa2fffb484f4d1cccdd46d00640daa1c
changer707 <53569122+changer707@users.noreply.github.com>
2019-10-19 14:50:49 +0800

    2019.10.19
    在已知触须序号k的前提下，完成触须的更新和轨迹记录

--- updatetentacle/IndexCarpoint.m
+++ updatetentacle/IndexCarpoint.m
@@ -1,6 +1,6 @@
 %求第k条触须执行0.1s后的位置
 %输入：速度v，触须编号k
-%输出：位置增量dx，dy
+%输出：位置增量dx，dy;执行长度对应的角度the_k
 function [dx,dy,the_k]=IndexCarpoint(v,k,r)
 if k==41
     dx=0;
--- updatetentacle/MarkPath.m
+++ updatetentacle/MarkPath.m
@@ -1,20 +1,25 @@
-%实现每次触须轨迹的记录
-function [ ]=MarkPath(r,k,the_k,W,H)
-
+%记录每次触须的轨迹
+%输入：半径集r，触须编号k，旋转中心（车点）：（W,H）， 速度V ，旋转角rotate_the
+function [ ]=MarkPath(r,k,W,H,V,rotate_the)
+L=0.1*V;
+the_k=L./r(k);
         if k<41
             the=(pi/2-the_k):2*pi/36000:pi/2;  %取样点要多，不够会导致两边看起来不对称
             y=r(k)*cos(the)+H;
             x=r(k)*sin(the)-r(k)+W;
-            plot(x,y);
+           [x1,y1]=TentacleRotate(x,y,W,H,rotate_the);
+            plot(x1,y1,'r');
         elseif k==41        %中间一条为直线
             y=0+H:0.01:l(k)+H;
             x=0+W;
-            plot(x,y,'-b');
+            [x1,y1]=TentacleRotate(x,y,W,H,rotate_the);
+            plot(x1,y1,'-r');
         else
             the=0.5*pi:2*pi/36000:(0.5*pi+the_k);
             y=r(k)*cos(the)+H;
             x=r(k)*sin(the)-r(k)+W; %因为r定义时有正负分类，故这里上下公式一样，不用分类讨论
-            plot(x,y);
+            [x1,y1]=TentacleRotate(x,y,W,H,rotate_the);
+            plot(x1,y1,'r');
         end
    
 %    text(W,H,['(',num2str(W),',',num2str(H),')']);
